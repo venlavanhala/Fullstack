1ba5f8187c48cb1334f9864ebaa28071f48859df
venlavanhala <venla.e.vanhala@gmail.com>
2024-07-17 23:37:35 +0300

    Modify App file

--- osa1/part1/src/App.jsx
+++ osa1/part1/src/App.jsx
@@ -1,7 +1,32 @@
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+const Hello = (props) => {
+  console.log(props.name)
+  return (
+    <div>
+      <p>Hello {props.name}!</p>
+      <p>You are {props.age} years old.</p>
+    </div>
+  )
+}
 
+const Goodbye = () => {
+  const day = new Date()
+  console.log(day.toString())
+  return (
+    <div>
+      <p>Goodbye, see you on {day.toString()}!</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const age1 = 25
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name="Venla" age={23}/>
+      <Hello name="Vanhala" age={age1}/>
+      <Goodbye />
+    </>
+  )
+}
 export default App
