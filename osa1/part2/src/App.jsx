e898488461a3caaa7393e64534aff0be791f4ffa
venlavanhala <venla.e.vanhala@gmail.com>
2024-07-18 20:21:24 +0300

    Modify part2 and do own tests

--- osa1/part2/src/App.jsx
+++ osa1/part2/src/App.jsx
@@ -1,6 +1,5 @@
 
 const Header = (props) => {
- // renderöi kurssin nimen
   return (
     <>
       <h1>{props.course}</h1>
@@ -17,12 +16,14 @@ const Part = (props) => {
 }
 
 const Content = (props) => {
-  return (
-    <div>
-      <Part name={props.parts[0]} number={props.exercises[0]}/>
-      <Part name={props.parts[1]} number={props.exercises[1]}/>
-      <Part name={props.parts[2]} number={props.exercises[2]}/>
-  </div>
+  props.parts.map(propsit => {
+    console.log(propsit.name)
+    console.log(propsit.exercises)
+    return (
+      <div>
+        <Part name={propsit.name} number={propsit.exercises}/>
+      </div>
+    )}
   )
 }
 
@@ -34,20 +35,34 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
+  /*const course = 'Half Stack application development'
+  /*const part1 = 'Fundamentals of React'
   const exercises1 = 10
   const part2 = 'Using props to pass data'
   const exercises2 = 7
   const part3 = 'State of a component'
-  const exercises3 = 14
+  const exercises3 = 14*/
+
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]} />
-      <Total exercises={[exercises1, exercises2, exercises3]} />
+      <Content parts={[part1, part2, part3]} />
     </div>
+    // <Total exercises={[exercises1, exercises2, exercises3]} />
   )
 }
 
---
+++ osa1/testing/test.js
@@ -0,0 +1,12 @@
+const t = [1, -1, 3]
+
+console.log(t.length) // tulostuu 3
+console.log(t[1])     // tulostuu -1
+
+t.push(5)             // lisätään taulukkoon luku 5
+
+console.log(t.length) // tulostuu 4
+
+t.forEach(value => {
+  console.log(value)  // tulostuu 1, -1, 3, 5 omille riveilleen
+})         
